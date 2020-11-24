3b33d02eee754b8d68c117ea1ccc1e5991aa5184
Vu Tran <vu.tran@alta.com.vn>
2020-11-24 17:36:15 +0700

    add meteor

---
+++ Assets/VFX s/vfx Meteor/ProjectileMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public float speed;
+    public GameObject impactPrefab;
+    public List<GameObject> trails;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.position += transform.forward * (speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if (impactPrefab != null)
+        {
+            var vfxImpact = Instantiate(impactPrefab, pos, rot) as GameObject;
+            Destroy(vfxImpact,5);
+        }
+
+        // Tách trail ra 
+        if (trails.Count > 0)
+        {
+            foreach(GameObject trail in trails)
+            {
+                trail.transform.parent = null;
+                var ps = trail.GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Stop();
+                    Destroy(ps.gameObject, ps.main.duration * ps.main.startLifetime.constantMax);
+                }
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/VFX s/vfx Meteor/SpawnMeteor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMeteor : MonoBehaviour
+{
+    public GameObject vfxMeteorPrefab;
+    public Transform startPoint;
+    public float startDelta;
+    public Transform endPoint;
+    public float endDelta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Spawn();
+    }
+
+    void Spawn()
+    {
+        //var startPosition = startPoint.position;
+        //var endPosition = endPoint.position;
+        Vector3 startVector = new Vector3(Random.Range(-1 * startDelta, startDelta), startPoint.position.y, Random.Range(-1 * startDelta, startDelta));
+        Vector3 endVector = new Vector3(Random.Range(-1 * endDelta, endDelta), endPoint.position.y, Random.Range(-1 * endDelta, endDelta));
+
+        GameObject go_vfxMeteor = Instantiate(vfxMeteorPrefab, startVector, Quaternion.identity) as GameObject;
+
+        RotateTo(go_vfxMeteor, endVector);
+    }
+
+    void RotateTo (GameObject go, Vector3 destination)
+    {
+        var direction = destination - go.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        go.transform.localRotation = Quaternion.Lerp(go.transform.localRotation, rotation, 1f);
+    }
+}
